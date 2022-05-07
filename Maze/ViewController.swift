0bc20091a94c3c7698c07f605cea12e74b63cfaf
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-05-08 03:17:30 +0900

    土台完成

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let screenSize = UIScreen.main.bounds.size
+    
+    let maze = [
+        [1,0,0,0,1,0],
+        [1,0,1,0,1,0],
+        [3,0,1,0,1,0],
+        [1,1,1,0,0,0],
+        [0,0,1,0,0,0],
+        [0,1,1,0,1,0],
+        [0,1,1,0,1,0],
+        [0,0,0,0,1,0],
+        [0,1,1,0,0,0],
+        [0,0,1,1,1,2],
+    ]
 
+    var startView: UIView!
+    var goalView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let cellWidth = screenSize.width / CGFloat(maze[0].count)
+        let cellHeight = screenSize.height / CGFloat(maze.count)
+        
+        let cellOffsetX = cellWidth / 2
+        let cellOffsetY = cellHeight / 2
+        
+        for y in 0 ..< maze.count {
+            for x in 0 ..< maze[y].count{
+                switch maze[y][x]{
+                case 1:
+                    let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    wallView.backgroundColor = .black
+                    view.addSubview(wallView)
+                case 2:
+                    startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    startView.backgroundColor = .green
+                    view.addSubview(startView)
+                case 3:
+                    goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    goalView.backgroundColor = .red
+                    view.addSubview(goalView)
+                default: break
+                }
+            }
+        }
     }
 
+    func createView(x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat, offsetY: CGFloat) -> UIView{
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        let view = UIView(frame: rect)
+        
+        let center = CGPoint(x: offsetX + width * CGFloat(x), y: offsetY + height * CGFloat(y))
+        
+        view.center = center
+        
+        return view
+    }
 
 }
 
